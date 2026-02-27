421529140bb042ecafbe2d6d5c788a8432d38dcb
Vlad Danylyshyn <vladuslavsd@gmail.com>
2026-02-27 18:50:40 +0200

    Basic server && parallelism

--- server/cpp/include/server.h
+++ server/cpp/include/server.h
@@ -2,7 +2,50 @@
 #define SERVER_SERVER_H
 
 #include <string>
+#include <boost/asio.hpp>
+#include <boost/beast/websocket.hpp>
 
-void launch(const std::string& addr = "0.0.0.0", int port = 8080);
+using boost::asio::ip::tcp;
+namespace websocket = boost::beast::websocket;
+
+using websocket_type = std::shared_ptr<websocket::stream<tcp::socket>>;
+
+struct socket_t {
+private:
+    websocket_type ws;
+
+public:
+    socket_t() = default;
+
+    socket_t(const socket_t&) = delete;
+    socket_t& operator=(const socket_t&) = delete;
+
+    socket_t(socket_t&&) = default;
+    socket_t& operator=(socket_t&&) = default;
+
+    void set_ws(websocket_type websocket_stream) { ws = std::move(websocket_stream); }
+
+    bool empty() const { return !ws; }
+
+    ~socket_t() = default;
+};
+
+class server_t {
+    std::string addr = "0.0.0.0";
+    int port = 8080;
+
+    socket_t sender;
+    socket_t viewer;
+public:
+    server_t() = default;
+    explicit server_t(std::string addr, int port): addr(addr), port(port) {}
+
+    void launch();
+    void log_participants(const std::string& buf, websocket_type socket);
+
+    bool everybody() const { return !(sender.empty() || viewer.empty()); }
+    [[nodiscard]] socket_t& get_sender() { return sender; }
+    [[nodiscard]] socket_t& get_viewer() { return viewer; }
+};
 
 #endif //SERVER_SERVER_H
--- server/cpp/src/main.cpp
+++ server/cpp/src/main.cpp
@@ -3,6 +3,13 @@
 #include "server.h"
 
 int main(int argc, char** argv) {
-    launch();
+    server_t s;
+
+    if (argc == 3) {
+        std::string addr = argv[1];
+        int port = std::stoi(argv[2]);
+    }
+
+    s.launch();
     return 0;
 }
